dd21e34909a5ff7389fa89d432f15f22d5cbc7c0
Rdn-M <muhammadreadone96@gmail.com>
2022-04-06 11:16:41 +0700

    create new table

---
+++ app/Database/Migrations/2022-04-06-035938_Pegawai.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Pegawai extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+
+            'id' => [
+
+                'type' => 'int',
+                'constraint' => 11,
+                'unsigned'=>true,
+                'auto_increment'=>true
+
+            ],
+            'nama' => [
+                'type' => 'varchar',
+                'constraint'=>100,
+            ],
+            'email' => [
+                'type' => 'varchar',
+                'constraint' =>100,
+                'unique'=>true
+            ],
+            'bidang' => [
+                'type'=>'ENUM',
+                'constraint'=>['finance','marketing','hr']
+            ],
+            'alamat' => [
+                'type' => 'varchar',
+                'constraint' =>100
+            ]
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('pegawai');
+        
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('pegawai');
+    }
+}
